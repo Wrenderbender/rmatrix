87a1f88230b742548ade24ea672a56ca4a0579ad
W <lachlananon@gmail.com>
2022-07-20 22:00:17 +1000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::fmt::Display;
+
+mod logic;
+
+fn main() {
+    let eng = ['!', '?', '@']; 
+    let blank = [' '];
+    let scrn = logic::gen_screen(&eng.to_vec(), 15, 15);
+    let col = logic::gen_column(&blank.to_vec(), scrn[0].len());
+    let cycle = logic::next_line(scrn[0].clone(), col[0], (scrn.len() -1) as i32);
+}
+
+fn pretty_print<T: Display>(x:&Vec<Vec<T>>){
+    for i in x{
+        for j in i{
+            print!("{}", j);
+        }
+        println!();
+    }
+}
